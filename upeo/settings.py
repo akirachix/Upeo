760ff00ab7ec1aa27556be9c7fedbc88978979ae
Mercy-Jepchirchir <mjepchirchirkemboi@gmail.com>
2022-10-04 23:31:36 +0300

    added models, and classes in Admin

--- upeo/settings.py
+++ upeo/settings.py
@@ -74,10 +74,20 @@ WSGI_APPLICATION = 'upeo.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'myproject',
+        'USER': 'myprojectuser',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': 5432,
     }
 }
 
--- upeoapp/models.py
+++ upeoapp/models.py
@@ -1,3 +1,38 @@
 from django.db import models
+# Create your models here
 
-# Create your models here.
+class Student(models.Model):
+      first_name = models.CharField(max_length=15,null=True)
+      last_name = models.CharField(max_length=15,null=True)
+      admission_number = models.CharField(max_length=10,null=True)
+      pin = models.IntegerField()
+      
+class Notification(models.Model):
+      status = models.CharField(max_length = 6,null=True)
+      STATUS_CHOICE = (
+        ('read','read'),
+        ('unread','unread'),
+    )
+
+      message = models.CharField(max_length = 15,null=True)
+      date = models.DateTimeField()
+      origin = models.CharField(max_length = 6,null=True)
+      destination = models.ForeignKey(to=Student,on_delete=models.CASCADE)
+      title = models.CharField(max_length=15,null=True)
+      
+class Topic(models.Model):
+    title = models.CharField(max_length = 10)
+    questions = models.ForeignKey(to=Student,on_delete=models.CASCADE)
+    date = models.DateTimeField()
+    
+      
+class Revision_questions(models.Model):
+      title = models.CharField(max_length = 10)
+      date = models.DateTimeField()
+      message = models.CharField(max_length = 20)
+      answers = models.ForeignKey(to=Student,on_delete=models.CASCADE)
+      
+class Answers(models.Model):
+      date = models.DateTimeField()
+      message = models.CharField(max_length = 10)
+      questions = models.ForeignKey(to=Student,on_delete=models.CASCADE)
