6ae26fb429f09ec79cb03eb4a73b5e28ab4a6222
Mercy-Jepchirchir <mjepchirchirkemboi@gmail.com>
2022-10-07 17:14:39 +0300

    added views of a student

--- upeo/settings.py
+++ upeo/settings.py
@@ -26,8 +26,9 @@ SECRET_KEY = 'django-insecure-q97@m($g^)1-=o=$facxxya_j&13qb(z(-iaf+sryax7vmhk=9
 DEBUG = True
 
 ALLOWED_HOSTS = [
-    '197-232-143-191.eu.ngrok.io',
-    '127.0.0.1'
+    '6260-197-232-51-151.eu.ngrok.io',
+    '127.0.0.1',
+    
 ]
 
 
@@ -131,6 +132,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+# STATIC_ROOT = os.path.join(BASE_DIR, ‘static’)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
--- ussdApp/views.py
+++ ussdApp/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
+from django.http import HttpResponse
 
 # Create your views here.
+
+
+
+@csrf_exempt
+def index(request):
+    if request.method == 'POST':
+        session_id = request.POST.get('sessionId')
+        service_code = request.POST.get('serviceCode')
+        admission_number = request.POST.get('admissionnumber')
+        text = request.POST.get('text')
+
+        response = ""
+
+        if text == "":
+            response = "CON Welcome to QuickSoma. Enter your admission\n"
+            # response .= "1. My Account \n"
+            response += "1. My Admission Number"
+
+        elif text == "1":
+            response = "END My Admission number is {0}".format(admission_number)
+            
+        return HttpResponse(response)
+    return HttpResponse({"message": "this method requires a POST request"})
